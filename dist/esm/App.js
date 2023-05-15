ce0bad6db16b0c24693fea834582346de5d89c6f
Abiodun Michael <michaelmedewase@gmail.com>
2023-05-15 16:28:11 +0100

    fix css bug

--- dist/esm/App.js
+++ dist/esm/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import './App.css';
 function App() {
-    return (React.createElement(React.Fragment, null));
+    return React.createElement(React.Fragment, null);
 }
 export default App;
 //# sourceMappingURL=App.js.map
